e04eea5540306bdf1e73375a92c281e17a71cd41
Roger Franklin <rogerfranklin@outlook.com>
2015-09-30 20:30:24 +0100

    Added an IOS picker for which friends you want, will probably change again due to ux

--- App/components/MakeShuggestion.react.js
+++ App/components/MakeShuggestion.react.js
@@ -16,56 +16,99 @@ var {
   TextInput,
 } = React;
 
-var MakeShuggestion = React.createClass({
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  PickerIOS,
+} = React;
+
+class ShuggestForm extends React.Component {
+  
+  constructor() {
+  	super()
+		this.state = {
+			suggestion: "",
+  		friend: {
+  				name: "friend",
+  				id: null
+			}	
+		}	
+	}
+  
+  _sendSuggestion(){
+    
+  //  {
+  //  	"category":"movie", 
+  //  	"suggestion":"[name of movie]",
+  //  	"url":"[imdb url]",
+  //  	"fbname":"[name of friend]",
+  //  	"fbid":"[Facebook ID of friend]",
+  // 	 	"fromfbname":"[your name]",
+  // 		"fromfbid":"[your Facebook ID ]"
+  //  }
+    
+  }
+
+	_getImdbUrl(){
+  	//get IMDB url from ask jakub
+  }
+
+	_getFBFriends(){
+  	//get friends for fbgraph
+    
+    
+    return [{name:"John", id: "1"},{name: "geoff",id:"2"},{name: "kevin",id: 3}, {name: "paul", id: 4}, {name: "keith", id: 5}]
+  }
   
   render() {
     return (
-      <View>
-        <View style={styles.wrapper}>
-          <View style={styles.imageContainer}>
-            <Image source={{uri: 'http://shuggest.com/images/logo.jpg'}} style={styles.image} />
-          </View>
-          <View>
-            <Text>Make a shuggestion to a friend</Text>
-          </View>
-        </View>
-        <View>
-          <Text>I want to share a</Text>
-          <TextInput
-            style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-            value="movie"
-          />
-          <Text>Your suggestion</Text>
-          <TextInput
-            style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-            value="Suggestion"
-          />
-              
-        </View>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Make a shuggestion
+        </Text>
+  			<Text>Your suggestion</Text>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(suggestion) => this.setState({suggestion: suggestion})}
+          value={this.state.suggestion}
+        />
+        
+        <Text>To who?</Text>
+        <PickerIOS
+          selectedValue="Blah"
+          onValueChange={(carMake) => this.setState({friend: carMake})}>
+          {Object.keys(this._getFBFriends()).map((friend) => (
+            <PickerIOS.Item
+              key={friend}
+              value={friend}
+              label={this._getFBFriends()[friend].name}
+              />
+            )
+          )}
+        </PickerIOS>
+        
+        
+        
+
       </View>
     );
   }
-});
+};
 
 var styles = StyleSheet.create({
-  wrapper: {
-    alignItems: 'center',
-  },
-  imageContainer: {
+  container: {
     flex: 1,
-    alignItems: 'center',
     paddingTop: 25,
+    backgroundColor: '#F5FCFF',
   },
-  image: {
-    height: 42,
-    width: 200,
+  welcome: {
+    fontSize: 28,
+    textAlign: 'center',
+    margin: 10,
   },
-  suggestionContainer: {
-    flex: 1,
-    alignItems: 'stretch',
-    paddingTop: 25,
-  }
-
 });
-
-module.exports = MakeShuggestion;
+module.exports = ShuggestForm;
